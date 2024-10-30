67d76e3a4425d52a1f53bccf6f0a204aa7d58602
Kashaf Shaikh <kashaf@example.com>
2024-10-30 19:59:42 +0500

    updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -20,7 +20,7 @@ export default function About() {
               </h2>
               <p>
                 Welcome to our restaurant! We pride ourselves on offering a unique dining experience with dishes made from the freshest ingredients. Our chefs work tirelessly to bring authentic flavors to every plate, ensuring that each bite is a delightful experience. <br />
-                Whether you're enjoying a classic pizza or one of our specialty dishes, we guarantee you'll leave satisfied and eager to return. Join us for a meal that will tantalize your taste buds and warm your heart.
+                Whether you&rsquo;re enjoying a classic pizza or one of our specialty dishes, we guarantee you&rsquo;ll leave satisfied and eager to return. Join us for a meal that will tantalize your taste buds and warm your heart.
               </p>
               <button className="learn-more-button">Learn More</button>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import About from "./about/page";
 import Contact from "./contact/page";
 import Menu from "./menu/page";
-import Image from 'next/image';
+import Image from "next/image";
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
@@ -27,9 +27,9 @@ export default function Home() {
         {/* <Header /> */}
         <div className="header-overlay">
           <h1>
-            "Discover the 
+            &quot;Discover the 
             <br></br>
-            <span className="highlight">Taste of Happiness!"</span>
+            <span className="highlight">Taste of Happiness!&quot;</span>
           </h1>
           <p>
             Welcome to our restaurant where we serve the most delicious meals. Ready to explore?
